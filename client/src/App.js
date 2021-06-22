1db70fc35f3c70a93ac54746ec5a93a706e257e6
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-06-22 21:53:33 +0200

    add basic nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
   render(){
     return(
       <div className="App">
-        <NavBar/>
+        <NavBar user={this.state.user} setUser={this.setUser}/>
       </div>
     )
   }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { logout } from "../services/auth";
 
 export default function NavBar(props) {
+  const logoutHandler = () => {
+    logout().then(() => {
+      props.setUser(null);
+    });
+  };
+
   return (
     <div>
-      
+      <div>
+        <h1>MOOD</h1>
+      </div>
+      <div>
+        <ul>
+          {props.user ? (
+            <li>
+              <Link to="/" onClick={() => logoutHandler()}>
+                Log Out
+              </Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+              <li>Sign Up</li>
+            </>
+          )}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
